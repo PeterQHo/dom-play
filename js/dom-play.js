7bb366a527d9303088b5090feef1982b962f8d8e
PeterQHo <pho@nyc.yearup.org>
2022-07-21 10:06:59 -0700

    New Actors

---
+++ js/dom-play.js
@@ -0,0 +1,48 @@
+// alert("It's working!");
+
+// let actors = document.querySelectorAll(".hamlet");
+ 
+// console.log(actors);
+
+function myAlert(){
+    alert("I'm Clicked!");
+}
+
+let spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click", function(ev){
+        highlightActor(mySpan.dataset.actor);
+    });
+    // alert(mySpan.dataset.actor);
+}
+
+function highlightActor(actor){
+
+    for(const mySpan of spans){
+        if(actor == mySpan.dataset.actor){//current actor, highlight
+            mySpan.style.backgroundColor = 'yellow';
+        }else{//not current actor, remove highlight
+            mySpan.style.backgroundColor = 'white';
+        }
+    }
+
+
+    // alert("The current actor is: " + actor);
+}
+
+
+function highlight(el){
+
+    if(el.style.backgroundColor == "yellow"){
+        el.style.backgroundColor = "white";
+    }else{
+        el.style.backgroundColor = 'yellow';
+    }
+
+
+
+    
+}
